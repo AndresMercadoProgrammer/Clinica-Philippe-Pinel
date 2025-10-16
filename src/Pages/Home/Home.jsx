baccbc648213cac9f34f15af153167ee0df8eccd
andres mercado <andres95rino@gmail.com>
2025-10-16 15:43:20 -0300

    Corrijo mayúsculas en import

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Hero } from "./Hero/Hero";
-import { Servicios } from "./Servicios/servicios";
+import { Servicios } from "./Servicios/Servicios";
 import { Turnos } from "./Turnos/Turnos";
 import { About } from "./About/About";
 import { Contacto } from "./Contacto/Contacto";
