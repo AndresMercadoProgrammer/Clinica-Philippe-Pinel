159655384801edc66ffaced483517efd6d7daa5d
andres mercado <andres95rino@gmail.com>
2025-10-19 03:30:06 -0300

    add speedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <SpeedInsights />
+
     <App />
   </StrictMode>
 );
