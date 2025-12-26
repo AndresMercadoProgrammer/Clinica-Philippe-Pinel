56e7111bdf6412615664fc1455a2ef14f613775f
andres mercado <andres95rino@gmail.com>
2025-12-26 02:57:09 -0300

    cambio de portada

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import styles from "./Hero.module.css";
-import portada from "@assets/ImagenPortada.svg";
+
 export const Hero = () => {
   return (
     <>
       <section id="home" className={styles.heroContainer}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}> TU MENTE TAMBIEN MERECE CUIDADO</h1>
+        <div className={styles.overlay}></div>
+        <div className={styles.heroContent}>
+          <div><h1 className={styles.title}> TU MENTE TAMBIEN MERECE CUIDADO</h1>
           <h3 className={styles.parrafo}>
             Ética Profesional, Trabajo Interdisciplinario y Excelencia
-          </h3>
-        </div>
-        <div className={styles.imageContainer}>
-          <img className={styles.img} src={portada} alt="Dos manos tomadas" />
+          </h3></div>
+          
+          <a href="#turnos">
+            <button className={styles.btnAction}>Agendar Consulta</button>
+          </a>
         </div>
       </section>
-      <a href="#turnos">  <button className={styles.btnAction}>Agendar Consulta</button> </a>
     </>
   );
 };
