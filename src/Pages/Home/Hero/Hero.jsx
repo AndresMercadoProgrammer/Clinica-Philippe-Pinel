24107ff7ea3524ceab38389ee1ee4661089cdb4b
andres mercado <andres95rino@gmail.com>
2025-12-26 03:10:42 -0300

    portada desktop fix

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -7,10 +7,12 @@ export const Hero = () => {
       <section id="home" className={styles.heroContainer}>
         <div className={styles.overlay}></div>
         <div className={styles.heroContent}>
-          <div><h1 className={styles.title}> TU MENTE TAMBIEN MERECE CUIDADO</h1>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>TU MENTE TAMBIEN MERECE CUIDADO</h1>
           <h3 className={styles.parrafo}>
             Ética Profesional, Trabajo Interdisciplinario y Excelencia
-          </h3></div>
+          </h3>
+          </div>
           
           <a href="#turnos">
             <button className={styles.btnAction}>Agendar Consulta</button>
